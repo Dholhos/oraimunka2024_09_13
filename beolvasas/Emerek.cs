89fef6371b69de9e6611338c8248095155963dad
Dholhos <113104720+Dholhos@users.noreply.github.com>
2024-09-13 13:39:25 +0200

    Add files via upload

---
+++ beolvasas/Emerek.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beolvasas
+{
+    internal class Emerek
+    {
+        string kerNev, vezNev;
+        int IDelso, Eletkor;
+
+        public Emerek(string kerNev, string vezNev, int iDelso, int eletkor)
+        {
+            this.kerNev = kerNev;
+            this.vezNev = vezNev;
+            IDelso = iDelso;
+            Eletkor = eletkor;
+        }
+
+        public string KerNev { get => kerNev; set => kerNev = value; }
+        public string VezNev { get => vezNev; set => vezNev = value; }
+        public int IDelso1 { get => IDelso; set => IDelso = value; }
+        public int Eletkor1 { get => Eletkor; set => Eletkor = value; }
+        
+    }
+}
---
+++ beolvasas/Program.cs
@@ -0,0 +1,43 @@
+﻿using beolvasas;
+using System;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            StreamReader sr = new StreamReader("people.csv");
+            string sor;
+            string kerNev;
+            string vezNev, ID1, ID2;
+            int eletKor;
+
+            int legfiatalabb = 100, atlagKor = 0, seged = 0;
+            while ((sor = sr.ReadLine()) != null)
+            {
+                sor = sr.ReadLine();
+                list.Add(sor);
+                kerNev = sor.Split(";")[0];
+                vezNev = sor.Split(";")[1];
+                ID1 = sor.Split(";")[2].Split('-')[0];
+                ID2 = sor.Split(";")[2].Split('-')[2];
+                eletKor = int.Parse(sor.Split(';')[2].Split('-')[1]);
+                atlagKor += eletKor;
+                seged++;
+            }
+            Console.WriteLine(atlagKor / seged);
+            Console.WriteLine(atlagKor);
+            Console.WriteLine(seged);
+            int eletkor = 0;
+            foreach (var item in list)
+            {
+
+            }
+            //Console.WriteLine(legfiatalabb);
+        }
+    }
+}
